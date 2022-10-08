22906063b8c95f5cd3bd880c3cd457bceb8df3e3
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-08 23:49:44 +0900

    refactor: GameManager, RoundManager 클래스 분리

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,10 +1,10 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.model.RacingCar;
 import racingcar.model.RacingCarNames;
 import racingcar.model.RacingCars;
 import racingcar.model.Round;
+import racingcar.service.GameManager;
+import racingcar.service.RoundManager;
 import racingcar.util.Message;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -12,25 +12,28 @@ import racingcar.view.OutputView;
 public class RacingGame {
 
     private final RacingCars racingCars;
-    private RacingCars winners;
     private Round round;
+    private RoundManager roundManager;
+    private GameManager gameManager;
 
     public RacingGame() {
         this.racingCars = new RacingCars();
-        this.winners = new RacingCars();
     }
 
     public void play() {
+        init();
+        roundManager.playRound();
+        OutputView.print(Message.RESULT.getMessage());
+        gameManager.calculateResult();
+        gameManager.printResult();
+    }
+
+    private void init() {
         inputRacingCarName();
         inputRound();
-        OutputView.print(Message.RESULT.getMessage());
-        for(int i=0; i<round.getRound(); i++) {
-            playRound();
-            printRound();
-            OutputView.printLine();
-        }
-        calculateResult();
-        printResult();
+
+        this.roundManager = new RoundManager(round, racingCars);
+        this.gameManager = new GameManager(racingCars);
     }
 
     private void inputRacingCarName() {
@@ -50,47 +53,5 @@ public class RacingGame {
             OutputView.print(e.getMessage());
             inputRound();
         }
-    }
-
-    private void playRound() {
-        for(int i=0; i<racingCars.size(); i++) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            racingCars.get(i).moveOrNot(randomNumber);
-        }
-    }
-
-    private void printRound() {
-        for(int i=0; i< racingCars.size(); i++) {
-            racingCars.get(i).printPosition();
-        }
-    }
-
-    private void calculateResult() {
-        int maxPosition = 0;
-
-        for(int i=0; i<racingCars.size(); i++) {
-            RacingCar racingCar = racingCars.get(i);
-            maxPosition = getMaxPositionAndInitWinnerList(racingCar, maxPosition);
-            addWinner(racingCar, maxPosition);
-        }
-    }
-
-    private int getMaxPositionAndInitWinnerList(RacingCar racingCar, int maxPosition) {
-        if(racingCar.getPosition() > maxPosition) {
-            winners = new RacingCars();
-            return racingCar.getPosition();
-        }
-
-        return maxPosition;
-    }
-
-    private void addWinner(RacingCar racingCar, int maxPosition) {
-        if(racingCar.getPosition() == maxPosition) {
-            winners.add(racingCar);
-        }
-    }
-
-    private void printResult() {
-        OutputView.print(String.format(Message.WINNER.getMessage(), winners.toString()));
     }
 }
